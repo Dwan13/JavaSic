75eb2309b3ea6d774a196b46c1df76138d9c6009
Dwan Veloza <dwanveloza@gmail.com>
2025-03-07 23:12:52 -0500

    Update UserRepository.java

--- form-back/answers-backend/src/main/java/com/springboot/backend/felipe/usersanswers/answers_backend/repositories/UserRepository.java
+++ form-back/answers-backend/src/main/java/com/springboot/backend/felipe/usersanswers/answers_backend/repositories/UserRepository.java
@@ -4,10 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.springboot.backend.felipe.usersanswers.answers_backend.entities.User;
 
@@ -15,13 +12,4 @@ public interface UserRepository extends CrudRepository<User, Long> {
         Page<User> findAll(Pageable pageable);
 
         Optional<User> findByUsername(String name);
-
-        @Modifying
-        @Query("UPDATE User u SET u.failed_attempts = :failedAttempts WHERE u.id = :id")
-        void updateFailedAttempts(@Param("failedAttempts") int failedAttempts, @Param("id") Long id);
-
-        @Modifying
-        @Query("UPDATE User u SET u.locked_account = :locked WHERE u.id = :id")
-        void updateLockedAccount(@Param("locked") boolean locked, @Param("id") Long id);
-
 }
